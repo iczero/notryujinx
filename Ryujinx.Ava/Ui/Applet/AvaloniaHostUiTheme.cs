a617f21b117168afb3669f5c04927269fc256b97
Emmanuel Hansen <emmausssss@gmail.com>
2021-12-26 19:26:48 +0000

    fix font crash when loading software keyboard

--- Ryujinx.Ava/Ui/Applet/AvaloniaHostUiTheme.cs
+++ Ryujinx.Ava/Ui/Applet/AvaloniaHostUiTheme.cs
@@ -15,7 +15,7 @@ namespace Ryujinx.Ava.Ui.Applet
     {
         public AvaloniaHostUiTheme(MainWindow parent)
         {
-            FontFamily               = parent.FontFamily.Name;
+            FontFamily               = OperatingSystem.IsWindows() && OperatingSystem.IsWindowsVersionAtLeast(10,0, 22000, 0) ? "Segoe UI Variable" : parent.FontFamily.Name;
             DefaultBackgroundColor   = BrushToThemeColor(parent.Background);
             DefaultForegroundColor   = BrushToThemeColor(parent.Foreground);
             DefaultBorderColor       = BrushToThemeColor(parent.BorderBrush);
