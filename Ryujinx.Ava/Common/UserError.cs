tickernelz/Ryujinx

add avalonia ui

Ryujinx.Ava/Helper/UserError.cs
@@ -1,0 +1,39 @@
+﻿namespace Ryujinx.Ava.Helper
+{
+    /// <summary>
+    ///     Represent a common error that could be reported to the user by the emulator.
+    /// </summary>
+    public enum UserError
+    {
+        /// <summary>
+        ///     No error to report.
+        /// </summary>
+        Success = 0x0,
+
+        /// <summary>
+        ///     No keys are present.
+        /// </summary>
+        NoKeys = 0x1,
+
+        /// <summary>
+        ///     No firmware is installed.
+        /// </summary>
+        NoFirmware = 0x2,
+
+        /// <summary>
+        ///     Firmware parsing failed.
+        /// </summary>
+        /// <remarks>Most likely related to keys.</remarks>
+        FirmwareParsingFailed = 0x3,
+
+        /// <summary>
+        ///     No application was found at the given path.
+        /// </summary>
+        ApplicationNotFound = 0x4,
+
+        /// <summary>
+        ///     An unknown error.
+        /// </summary>
+        Unknown = 0xDEAD
+    }
+}

Ryujinx.Ava/Ui/Controls/ProfileImageSelectionDialog.axaml.cs
@@ -1,0 +1,101 @@
+using Avalonia;
+using Avalonia.Controls;
+using Avalonia.Interactivity;
+using Avalonia.Markup.Xaml;
+using Ryujinx.Ava.Ui.Windows;
+using Ryujinx.HLE.FileSystem.Content;
+using SixLabors.ImageSharp;
+using SixLabors.ImageSharp.Processing;
+using System.IO;
+using Image = SixLabors.ImageSharp.Image;
+
+namespace Ryujinx.Ava.Ui.Controls
+{
+    public class ProfileImageSelectionDialog : Window
+    {
+        private readonly ContentManager _contentManager;
+
+        public bool FirmwareFound => _contentManager.GetCurrentFirmwareVersion() != null;
+
+        public byte[] BufferImageProfile { get; set; }
+
+        public ProfileImageSelectionDialog(ContentManager contentManager)
+        {
+            _contentManager = contentManager;
+            DataContext = this;
+            InitializeComponent();
+#if DEBUG
+            this.AttachDevTools();
+#endif
+        }
+
+        public ProfileImageSelectionDialog()
+        {
+            DataContext = this;
+            InitializeComponent();
+#if DEBUG
+            this.AttachDevTools();
+#endif
+        }
+
+        private void InitializeComponent()
+        {
+            AvaloniaXamlLoader.Load(this);
+        }
+
+        private async void Import_OnClick(object? sender, RoutedEventArgs e)
+        {
+            OpenFileDialog dialog = new();
+            dialog.Filters.Add(new FileDialogFilter
+            {
+                Name = "All Supported Formats", Extensions = {"jpg", "jpeg", "png", "bmp"}
+            });
+            dialog.Filters.Add(new FileDialogFilter {Name = "JPEG", Extensions = {"jpg", "jpeg"}});
+            dialog.Filters.Add(new FileDialogFilter {Name = "PNG", Extensions = {"png"}});
+            dialog.Filters.Add(new FileDialogFilter {Name = "BMP", Extensions = {"bmp"}});
+
+            dialog.AllowMultiple = false;
+
+            string[] image = await dialog.ShowAsync(this);
+
+            if (image.Length > 0)
+            {
+                string imageFile = image[0];
+
+                ProcessProfileImage(File.ReadAllBytes(imageFile));
+            }
+
+            Close();
+        }
+
+        private async void SelectFirmwareImage_OnClick(object? sender, RoutedEventArgs e)
+        {
+            if (FirmwareFound)
+            {
+                AvatarWindow window = new(_contentManager);
+
+                await window.ShowDialog(this);
+
+                BufferImageProfile = window.SelectedImage;
+
+                Close();
+            }
+        }
+
+
+        private void ProcessProfileImage(byte[] buffer)
+        {
+            using (Image image = Image.Load(buffer))
+            {
+                image.Mutate(x => x.Resize(256, 256));
+
+                using (MemoryStream streamJpg = new())
+                {
+                    image.SaveAsJpeg(streamJpg);
+
+                    BufferImageProfile = streamJpg.ToArray();
+                }
+            }
+        }
+    }
+}

iczero/notryujinx

Ryujinx.Ava/Common/UserError.cs
@@ -1,0 +1,39 @@
+﻿namespace Ryujinx.Ava.Common
+{
+    /// <summary>
+    ///     Represent a common error that could be reported to the user by the emulator.
+    /// </summary>
+    public enum UserError
+    {
+        /// <summary>
+        ///     No error to report.
+        /// </summary>
+        Success = 0x0,
+
+        /// <summary>
+        ///     No keys are present.
+        /// </summary>
+        NoKeys = 0x1,
+
+        /// <summary>
+        ///     No firmware is installed.
+        /// </summary>
+        NoFirmware = 0x2,
+
+        /// <summary>
+        ///     Firmware parsing failed.
+        /// </summary>
+        /// <remarks>Most likely related to keys.</remarks>
+        FirmwareParsingFailed = 0x3,
+
+        /// <summary>
+        ///     No application was found at the given path.
+        /// </summary>
+        ApplicationNotFound = 0x4,
+
+        /// <summary>
+        ///     An unknown error.
+        /// </summary>
+        Unknown = 0xDEAD
+    }
+}

Ryujinx.Ava/Ui/Controls/ProfileImageSelectionDialog.axaml.cs
@@ -1,0 +1,101 @@
+using Avalonia;
+using Avalonia.Controls;
+using Avalonia.Interactivity;
+using Avalonia.Markup.Xaml;
+using Ryujinx.Ava.Ui.Windows;
+using Ryujinx.HLE.FileSystem.Content;
+using SixLabors.ImageSharp;
+using SixLabors.ImageSharp.Processing;
+using System.IO;
+using Image = SixLabors.ImageSharp.Image;
+
+namespace Ryujinx.Ava.Ui.Controls
+{
+    public class ProfileImageSelectionDialog : StyleableWindow
+    {
+        private readonly ContentManager _contentManager;
+
+        public bool FirmwareFound => _contentManager.GetCurrentFirmwareVersion() != null;
+
+        public byte[] BufferImageProfile { get; set; }
+
+        public ProfileImageSelectionDialog(ContentManager contentManager)
+        {
+            _contentManager = contentManager;
+            DataContext = this;
+            InitializeComponent();
+#if DEBUG
+            this.AttachDevTools();
+#endif
+        }
+
+        public ProfileImageSelectionDialog()
+        {
+            DataContext = this;
+            InitializeComponent();
+#if DEBUG
+            this.AttachDevTools();
+#endif
+        }
+
+        private void InitializeComponent()
+        {
+            AvaloniaXamlLoader.Load(this);
+        }
+
+        private async void Import_OnClick(object sender, RoutedEventArgs e)
+        {
+            OpenFileDialog dialog = new();
+            dialog.Filters.Add(new FileDialogFilter
+            {
+                Name = "All Supported Formats", Extensions = {"jpg", "jpeg", "png", "bmp"}
+            });
+            dialog.Filters.Add(new FileDialogFilter {Name = "JPEG", Extensions = {"jpg", "jpeg"}});
+            dialog.Filters.Add(new FileDialogFilter {Name = "PNG", Extensions = {"png"}});
+            dialog.Filters.Add(new FileDialogFilter {Name = "BMP", Extensions = {"bmp"}});
+
+            dialog.AllowMultiple = false;
+
+            string[] image = await dialog.ShowAsync(this);
+
+            if (image.Length > 0)
+            {
+                string imageFile = image[0];
+
+                ProcessProfileImage(File.ReadAllBytes(imageFile));
+            }
+
+            Close();
+        }
+
+        private async void SelectFirmwareImage_OnClick(object sender, RoutedEventArgs e)
+        {
+            if (FirmwareFound)
+            {
+                AvatarWindow window = new(_contentManager);
+
+                await window.ShowDialog(this);
+
+                BufferImageProfile = window.SelectedImage;
+
+                Close();
+            }
+        }
+
+
+        private void ProcessProfileImage(byte[] buffer)
+        {
+            using (Image image = Image.Load(buffer))
+            {
+                image.Mutate(x => x.Resize(256, 256));
+
+                using (MemoryStream streamJpg = new())
+                {
+                    image.SaveAsJpeg(streamJpg);
+
+                    BufferImageProfile = streamJpg.ToArray();
+                }
+            }
+        }
+    }
+}