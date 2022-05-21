Soulize/Ryujinx

Add alpha test emulation on shader (but no shader specialisation yet...)

Ryujinx.Graphics.Gpu/Engine/Threed/StateUpdater.cs
@@ -1011,6 +1011,9 @@ namespace Ryujinx.Graphics.Gpu.Engine.Threed
                 _state.State.TexturePoolState.Address.Pack(),
                 _state.State.TexturePoolState.MaximumId,
                 (int)_state.State.TextureBufferIndex,
+                _state.State.AlphaTestEnable,
+                _state.State.AlphaTestFunc,
+                _state.State.AlphaTestRef,
                 _state.State.EarlyZForce,
                 GetDepthMode() == DepthMode.MinusOneToOne,
                 _state.State.VertexProgramPointSize,

iczero/notryujinx

Ryujinx.Graphics.Gpu/Engine/Threed/StateUpdater.cs
@@ -1226,7 +1226,10 @@ namespace Ryujinx.Graphics.Gpu.Engine.Threed
                 _state.State.ViewportTransformEnable == 0,
                 GetDepthMode() == DepthMode.MinusOneToOne,
                 _state.State.VertexProgramPointSize,
-                _state.State.PointSize);
+                _state.State.PointSize,
+                _state.State.AlphaTestEnable,
+                _state.State.AlphaTestFunc,
+                _state.State.AlphaTestRef);
         }
 
         private DepthMode GetDepthMode()