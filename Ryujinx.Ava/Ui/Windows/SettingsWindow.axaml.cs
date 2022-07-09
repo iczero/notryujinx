2c622af93e2e05ac269bc6eb5fab392e16a5ab31
Emmanuel Hansen <emmausssss@gmail.com>
2022-07-09 18:35:43 +0000

    Add gpu selector to Avalonia (#8)
    * Add gpu selector to avalonia settings
    
    * show backend label on window
    
    * some fixes
    
    * address review

--- Ryujinx.Ava/Ui/Windows/SettingsWindow.axaml.cs
+++ Ryujinx.Ava/Ui/Windows/SettingsWindow.axaml.cs
@@ -224,9 +224,9 @@ namespace Ryujinx.Ava.Ui.Windows
             }
         }
 
-        private void SaveButton_Clicked(object sender, RoutedEventArgs e)
+        private async void SaveButton_Clicked(object sender, RoutedEventArgs e)
         {
-            SaveSettings();
+            await SaveSettings();
 
             Close();
         }
@@ -237,14 +237,14 @@ namespace Ryujinx.Ava.Ui.Windows
             Close();
         }
 
-        private void ApplyButton_Clicked(object sender, RoutedEventArgs e)
+        private async void ApplyButton_Clicked(object sender, RoutedEventArgs e)
         {
-            SaveSettings();
+            await SaveSettings();
         }
 
-        private void SaveSettings()
+        private async Task SaveSettings()
         {
-            ViewModel.SaveSettings();
+            await ViewModel.SaveSettings();
 
             ControllerSettings?.SaveCurrentProfile();
 
