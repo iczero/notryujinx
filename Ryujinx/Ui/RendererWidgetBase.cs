natesworksdev/Ryujinx

WIP Vulkan implementation

Ryujinx/Ui/RendererWidgetBase.cs
@@ -68,6 +68,7 @@ namespace Ryujinx.Ui
         private InputManager _inputManager;
         private IKeyboard _keyboardInterface;
         private GraphicsDebugLevel _glLogLevel;
+        private string _gpuBackendName;
         private string _gpuVendorName;
 
         private int _windowHeight;
@@ -115,7 +116,12 @@ namespace Ryujinx.Ui
 
         public abstract void SwapBuffers();
 
-        public abstract string GetGpuVendorName();
+        protected abstract string GetGpuBackendName();
+
+        private string GetGpuVendorName()
+        {
+            return Renderer.GetHardwareInfo().GpuVendor;
+        }
 
         private void HideCursorStateChanged(object sender, ReactiveEventArgs<bool> state)
         {
@@ -222,7 +228,7 @@ namespace Ryujinx.Ui
             _windowWidth = evnt.Width * monitor.ScaleFactor;
             _windowHeight = evnt.Height * monitor.ScaleFactor;
 
-            Renderer?.Window.SetSize(_windowWidth, _windowHeight);
+            Renderer?.Window?.SetSize(_windowWidth, _windowHeight);
 
             return result;
         }
@@ -303,7 +309,7 @@ namespace Ryujinx.Ui
             }
 
             Renderer = renderer;
-            Renderer?.Window.SetSize(_windowWidth, _windowHeight);
+            Renderer.Window?.SetSize(_windowWidth, _windowHeight);
 
             if (Renderer != null)
             {
@@ -382,6 +388,7 @@ namespace Ryujinx.Ui
 
             Device.Gpu.Renderer.Initialize(_glLogLevel);
 
+            _gpuBackendName = GetGpuBackendName();
             _gpuVendorName = GetGpuVendorName();
 
             Device.Gpu.Renderer.RunLoop(() =>
@@ -423,6 +430,7 @@ namespace Ryujinx.Ui
 
                         StatusUpdatedEvent?.Invoke(this, new StatusUpdatedEventArgs(
                             Device.EnableDeviceVsync,
+                            _gpuBackendName,
                             dockedMode,
                             ConfigurationState.Instance.Graphics.AspectRatio.Value.ToText(),
                             $"Game: {Device.Statistics.GetGameFrameRate():00.00} FPS",

iczero/notryujinx

Ryujinx/Ui/RendererWidgetBase.cs
@@ -74,6 +74,7 @@ namespace Ryujinx.Ui
         private InputManager _inputManager;
         private IKeyboard _keyboardInterface;
         private GraphicsDebugLevel _glLogLevel;
+        private string _gpuBackendName;
         private string _gpuVendorName;
         private bool _isMouseInClient;
 
@@ -120,7 +121,12 @@ namespace Ryujinx.Ui
 
         public abstract void SwapBuffers(object image);
 
-        public abstract string GetGpuVendorName();
+        protected abstract string GetGpuBackendName();
+
+        private string GetGpuVendorName()
+        {
+            return Renderer.GetHardwareInfo().GpuVendor;
+        }
 
         private void HideCursorStateChanged(object sender, ReactiveEventArgs<bool> state)
         {
@@ -227,7 +233,7 @@ namespace Ryujinx.Ui
             WindowWidth = evnt.Width * monitor.ScaleFactor;
             WindowHeight = evnt.Height * monitor.ScaleFactor;
 
-            Renderer?.Window.SetSize(WindowWidth, WindowHeight);
+            Renderer?.Window?.SetSize(WindowWidth, WindowHeight);
 
             return result;
         }
@@ -308,7 +314,7 @@ namespace Ryujinx.Ui
             }
 
             Renderer = renderer;
-            Renderer?.Window.SetSize(WindowWidth, WindowHeight);
+            Renderer?.Window?.SetSize(WindowWidth, WindowHeight);
 
             if (Renderer != null)
             {
@@ -387,6 +393,7 @@ namespace Ryujinx.Ui
 
             Device.Gpu.Renderer.Initialize(_glLogLevel);
 
+            _gpuBackendName = GetGpuBackendName();
             _gpuVendorName = GetGpuVendorName();
 
             Device.Gpu.Renderer.RunLoop(() =>
@@ -432,6 +439,7 @@ namespace Ryujinx.Ui
                         StatusUpdatedEvent?.Invoke(this, new StatusUpdatedEventArgs(
                             Device.EnableDeviceVsync,
                             Device.GetVolume(),
+                            _gpuBackendName,
                             dockedMode,
                             ConfigurationState.Instance.Graphics.AspectRatio.Value.ToText(),
                             $"Game: {Device.Statistics.GetGameFrameRate():00.00} FPS ({Device.Statistics.GetGameFrameTime():00.00} ms)",