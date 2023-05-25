8126a56ec3fb5e54a6eacb5e7347a296d326d92b
MutantAura <domw0401@gmail.com>
2023-05-25 21:39:33 +0100

    Revert spacing

--- src/Ryujinx.Ava/UI/Models/StatusUpdatedEventArgs.cs
+++ src/Ryujinx.Ava/UI/Models/StatusUpdatedEventArgs.cs
@@ -13,6 +13,7 @@ namespace Ryujinx.Ava.UI.Models
         public string GameStatus { get; }
         public string GpuName { get; }
         public int ShaderCount { get; }
+        
         public StatusUpdatedEventArgs(bool vSyncEnabled, string volumeStatus, string gpuBackend, 
                                       string dockedMode, string aspectRatio,  string gameStatus, 
                                       string fifoStatus, string gpuName,      int shaderCount)
