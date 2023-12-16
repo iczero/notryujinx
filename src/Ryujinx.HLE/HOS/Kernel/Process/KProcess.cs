1abef7e26cfc453ec2ed1b4d12e2d85cdd9c55b7
svc64 <svcx80@icloud.com>
2023-12-16 20:03:25 +0200

    Fix debugger crash when using "info thread"

--- src/Ryujinx.HLE/HOS/Kernel/Process/KProcess.cs
+++ src/Ryujinx.HLE/HOS/Kernel/Process/KProcess.cs
@@ -1313,7 +1313,8 @@ namespace Ryujinx.HLE.HOS.Kernel.Process
             {
                 lock (_parent._threadingLock)
                 {
-                    return _parent._threads.Select(x => x.ThreadUid).ToArray();
+                    var threads = _parent._threads.Where(x => !x.TerminationRequested).ToArray();
+                    return threads.Select(x => x.ThreadUid).ToArray();
                 }
             }
 
@@ -1321,7 +1322,8 @@ namespace Ryujinx.HLE.HOS.Kernel.Process
             {
                 lock (_parent._threadingLock)
                 {
-                    return _parent._threads.FirstOrDefault(x => x.ThreadUid == threadUid);
+                    var threads = _parent._threads.Where(x => !x.TerminationRequested).ToArray();
+                    return threads.FirstOrDefault(x => x.ThreadUid == threadUid);
                 }
             }
 
