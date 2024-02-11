45ca124a485c6a6913da9179bd72bad57ca960dc
Kfollen93 <59298462+Kfollen93@users.noreply.github.com>
2024-02-11 10:00:39 -0800

    Create reusable SanitizeFileName method

--- src/Ryujinx.Ava/AppHost.cs
+++ src/Ryujinx.Ava/AppHost.cs
@@ -22,6 +22,7 @@ using Ryujinx.Common.Configuration;
 using Ryujinx.Common.Configuration.Multiplayer;
 using Ryujinx.Common.Logging;
 using Ryujinx.Common.SystemInterop;
+using Ryujinx.Common.Utilities;
 using Ryujinx.Graphics.GAL;
 using Ryujinx.Graphics.GAL.Multithreading;
 using Ryujinx.Graphics.Gpu;
@@ -272,24 +273,6 @@ namespace Ryujinx.Ava
             }
         }
 
-        private string SanitizeApplicationName(string applicationName)
-        {
-            StringBuilder sbAppName = new StringBuilder(applicationName);
-
-            HashSet<char> reservedChars = ['<', '>', ':', '"', '/', '\\', '|', '?', '*'];
-            for (char c = (char)0; c <= 31; c++)
-            {
-                reservedChars.Add(c);
-            }
-
-            foreach (char reservedChar in reservedChars)
-            {
-                sbAppName = sbAppName.Replace(reservedChar, '_');
-            }
-
-            return sbAppName.ToString();
-        }
-
         private void Renderer_ScreenCaptured(object sender, ScreenCaptureImageInfo e)
         {
             if (e.Data.Length > 0 && e.Height > 0 && e.Width > 0)
@@ -299,7 +282,7 @@ namespace Ryujinx.Ava
                     lock (_lockObject)
                     {
                         string applicationName = Device.Processes.ActiveApplication.Name;
-                        string sanitizedApplicationName = SanitizeApplicationName(applicationName);
+                        string sanitizedApplicationName = FileSystemUtils.SanitizeFileName(applicationName);
 
                         DateTime currentTime = DateTime.Now;
 
